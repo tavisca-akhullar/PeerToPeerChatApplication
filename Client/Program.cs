a43a405a80e5135aa5147aff94bd0f7e20783e06
Khullar <akhullar@internal.tavisca.com>
2019-07-31 21:39:16 +0530

    Done with peer to peer chat application

---
+++ Client/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+public class Client
+{
+    public static int Main(String[] args)
+    {
+        StartClient();
+        return 0;
+    }
+
+
+    public static void StartClient()
+    {
+        byte[] bytes = new byte[1024];
+
+        try
+        {
+            IPAddress ipAddress = IPAddress.Parse("172.16.5.194");
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(remoteEP);
+                Console.WriteLine("Socket connected to {0}",
+                sender.RemoteEndPoint.ToString());
+                Thread threadWrite = new Thread(() => Write(sender));
+                Thread threadRead = new Thread(() => Read(sender));
+                threadWrite.Start();
+                threadRead.Start();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public static void Read(Socket sender)
+    {
+        while (true)
+        {
+            string data = "";
+            byte[] bytes = new byte[1024];
+            int bytesRec = sender.Receive(bytes);
+            data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            Console.WriteLine("Client 2 {0}", data);
+        }
+    }
+
+    public static void Write(Socket sender)
+        {
+            while (true)
+            {
+                String message = Console.ReadLine();
+                byte[] msg = Encoding.ASCII.GetBytes(message);
+                sender.Send(msg);
+            }
+        }
+    }
---
+++ SocketListener/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+public class SocketListener
+{
+    public static int Main(String[] args)
+    {
+        StartServer();
+        return 0;
+    }
+    public static void StartServer()
+    {
+        IPAddress ipAddress = IPAddress.Parse("172.16.5.194");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+        try
+        {
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            Console.WriteLine("Waiting for a connection...");
+            Socket handler = listener.Accept();
+            Thread threadWrite = new Thread(() => Write(handler));
+            Thread threadRead = new Thread(() => Read(handler));
+            threadWrite.Start();
+            threadRead.Start();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\n Press any key to continue...");
+        Console.ReadKey();
+    }
+        public static void Read(Socket sender)
+        {
+            while (true)
+            {
+                String data = "";
+                byte[] bytes = new byte[1024];
+                int bytesRec = sender.Receive(bytes);
+                data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                Console.WriteLine("Client 1 {0}", data);
+            }
+        }
+
+    public static void Write(Socket sender)
+    {
+        while (true)
+        {
+            String message = Console.ReadLine();
+            byte[] msg = Encoding.ASCII.GetBytes(message);
+            sender.Send(msg);
+          }
+        }
+     }
+    
